ee4b90b0794deaf9aa97dc39a8fde51e33e4ade0
gaurav <gauravkumarsingh010103@gmail.com>
2024-12-02 20:23:54 +0530

    new mock data

--- src/Utils/EssenstialLink.js
+++ src/Utils/EssenstialLink.js
@@ -1,3 +1,3 @@
 export const IMG_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
  export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQLe_WuOUKelIF0TI27sP0x_PkdAd6CuXG8Q&s";
- export const MENU_URL="https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6321135&lng=77.4434152&restaurantId=707536&submitAction=ENTER";
+ export const MENU_URL=" https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6321135&lng=77.4434152&restaurantId=";
