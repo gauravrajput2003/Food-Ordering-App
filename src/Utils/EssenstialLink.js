a08bb0606048fd5f9a414fca952a97541e9174ad
gaurav <gauravkumarsingh010103@gmail.com>
2025-03-01 10:59:18 +0530

    safe apis

--- src/Utils/EssenstialLink.js
+++ src/Utils/EssenstialLink.js
@@ -1,6 +1,6 @@
 export const IMG_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL = "src\Utils\Red and Yellow Catering Flat Illustrative Food Place Logo.png";
 
- export const MENU_URL=" https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6321135&lng=77.4434152&restaurantId=";
+ export const MENU_URL=process.env.REACT_APP_MENU_URL;
  
   
